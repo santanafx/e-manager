177fc2c27620759592a3a049f4228a37a635d58e
Lucas Santana <santanafx@hotmail.com>
2023-09-05 05:38:49 -0300

    feat: adicionando menu lateral

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import './css/main.css'
+import Dashboard from './pages/Dashboard'
+import Produtos from './pages/Produtos/Index'
 
 function App() {
   return (
     <>
-      <Navbar />
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/produtos" element={<Produtos />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,3 +1,52 @@
+import { Link } from 'react-router-dom'
+
+import logo from '../../assets/images/logo.png'
+import menuOptGrid from '../../assets/images/menuOptGrid.png'
+import menuOptGraph from '../../assets/images/menuOptGraph.png'
+import chevronDown from '../../assets/images/chevronDown.png'
+
 export default function Navbar() {
-  return <div>navbar</div>
+  return (
+    <section className="navbar__menu">
+      <img className="navbar__menu__logo__img" src={logo} alt="Logo" />
+      <nav>
+        <div className="navbar__menu__list">
+          <Link
+            to="/dashboard"
+            className="navbar__menu__list__item"
+            tabIndex={-1}
+          >
+            <img
+              className="navbar__menu__list__item__icon__img"
+              src={menuOptGrid}
+              alt="Menu option"
+            />
+            <span>Dashboard</span>
+            <img
+              className="navbar__menu__list__item__arrow__img"
+              src={chevronDown}
+              alt="Menu arrow"
+            />
+          </Link>
+          <Link
+            to="/produtos"
+            className="navbar__menu__list__item"
+            tabIndex={-2}
+          >
+            <img
+              className="navbar__menu__list__item__icon__img"
+              src={menuOptGraph}
+              alt="Menu option"
+            />
+            <span>Produtos</span>
+            <img
+              className="navbar__menu__list__item__arrow__img"
+              src={chevronDown}
+              alt="Menu arrow"
+            />
+          </Link>
+        </div>
+      </nav>
+    </section>
+  )
 }
